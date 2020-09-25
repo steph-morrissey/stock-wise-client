ac616046b31550a2cd187208da02dbf724d2391e
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-09-25 22:42:31 +0100

    Updates

---
+++ precache-manifest.90caf1fdb0c8f94d086d5767324bcfde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753c7a91afcd47721ad863d56e791c68",
+    "url": "/stock-wise-client/index.html"
+  },
+  {
+    "revision": "77e1a47bfb2fbd1e1201",
+    "url": "/stock-wise-client/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "8fad4ebf12251d114565",
+    "url": "/stock-wise-client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "77e1a47bfb2fbd1e1201",
+    "url": "/stock-wise-client/static/js/2.aa599d48.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/stock-wise-client/static/js/2.aa599d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fad4ebf12251d114565",
+    "url": "/stock-wise-client/static/js/main.9634c8c5.chunk.js"
+  },
+  {
+    "revision": "febda8e58563b6e31ff0",
+    "url": "/stock-wise-client/static/js/runtime-main.2e32de98.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/stock-wise-client/precache-manifest.90caf1fdb0c8f94d086d5767324bcfde.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stock-wise-client/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
