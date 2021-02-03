dc6cb57837b7ec2fff90f004d1a25081f74e3a64
Stephanie Morrissey <sgmorrissey098@gmail.com>
2021-02-03 12:42:53 +0000

    heroku domain name update

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,11 +1,11 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8000'
-    : 'https://infinite-tor-17640.herokuapp.com';
+    : 'https://sheltered-citadel-55036.herokuapp.com';
 
 export const LOGIN_URL = `${BASE_URL}/auth/login`;
 export const REGISTER_URL = `${BASE_URL}/auth/register`;
 export const CATEGORIES_URI = `${BASE_URL}/api/categories`;
 export const SUPPLIERS_URI = `${BASE_URL}/api/suppliers`;
 export const PRODUCTS_URI = `${BASE_URL}/api/products`;
-export const DASHBOARD_URI = `${BASE_URL}/api/dashboard`
+export const DASHBOARD_URI = `${BASE_URL}/api/dashboard`;
